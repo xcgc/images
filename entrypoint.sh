1d113d3770426c45a960e3607c3277b64f4812f5
xcgc <74693042+xcgc@users.noreply.github.com>
2021-02-21 11:11:08 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,6 @@
 
 cd /home/container
 
-java -version
-
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
