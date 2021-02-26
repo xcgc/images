acd12c00a4b77a74f92a35cc8b3432d3e1dad485
xcgc <74693042+xcgc@users.noreply.github.com>
2021-02-26 18:17:50 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,4 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-eval ${MODIFIED_STARTUP}
+${MODIFIED_STARTUP}
